ca6b7b3e518334cc180ecb38e3b7bc375adc4e0d
AleksandarFa <aleksandar.fa@vivifyideas.com>
2021-05-24 14:44:52 +0200

    Added search filter and field

--- src/config/common.py
+++ src/config/common.py
@@ -321,7 +321,8 @@ THUMBNAIL_ALIASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.OrderingFilter'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.OrderingFilter',
+                                'rest_framework.filters.SearchFilter'],
     'PAGE_SIZE':
     int(os.getenv('DJANGO_PAGINATION_LIMIT', 18)),
     'DATETIME_FORMAT':
